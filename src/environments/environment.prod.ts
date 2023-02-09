4a0a7656147456b89736dd959d512d205851a6aa
Manoj Gowda <manojgowdakr28@gmail.com>
2023-02-09 17:41:06 +0530

    fix: fixed the backend uri in env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   intermediate_server_url: 'https://intermediateserver-todo.onrender.com',
-  backend_server_url: 'https://server-todo-rcrp.onrender.com',
+  backend_server_url: 'https://server-todo-6evj.onrender.com',
 };
