d5483100371c0a43b440225d9595a1c4239c1f87
Manoj Gowda <manojgowdakr28@gmail.com>
2023-02-09 16:01:41 +0530

    fix: fixed IS uri for env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  intermediate_server_url: 'http://localhost:3000',
-  backend_server_url: 'http://localhost:5000',
+  intermediate_server_url: 'https://intermediateserver-todo.onrender.com',
+  backend_server_url: 'https://server-todo-rcrp.onrender.com',
 };
